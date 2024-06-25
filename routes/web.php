0184c50562e7b2c300dd7950068581103f17d63d
haweil <92003311+haweil@users.noreply.github.com>
2024-06-25 13:25:02 +0300

    "Rename Dashboard"

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\ProfileController;
 require __DIR__.'/auth.php';
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
