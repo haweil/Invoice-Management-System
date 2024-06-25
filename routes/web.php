048775e6b74499c384a8c61d9b63594e6cfc26e7
haweil <92003311+haweil@users.noreply.github.com>
2024-06-25 12:01:40 +0300

    "Setup Authentication"

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+require __DIR__.'/auth.php';
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::get('/{page}', [AdminController::class, 'index']);
